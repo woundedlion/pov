15972f9e932b1e9efd5d4a48e9bdd14bc98beb03
Gabriel Levy <spam_gabe@yahoo.com>
2015-07-27 10:33:13 -0700

    initial commit

---
+++ Holosphere.ino
@@ -0,0 +1,32 @@
+#include <FastLED.h>
+#include "led.h"
+
+namespace {
+  const uint32_t RPM = 240;
+  const uint32_t NUM_PIXELS = 40;
+ 
+  Spinner<40, 20> spinner;
+  Grid<40, 20, 4> grid;
+  
+  const Effect* effects[] = {
+    &spinner, 
+    &grid,
+  };
+  
+  CRGB leds[NUM_PIXELS];
+  POVDisplay<NUM_PIXELS> pov(leds, RPM);
+}
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  unsigned long i = 0;
+  while (true) { 
+    pov.show(*effects[i], 5000);
+    i = (i + 1) % (sizeof(effects) / sizeof(Effect *)); 
+  }
+}
+
+
