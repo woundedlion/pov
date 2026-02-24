79b2e341911dae730bb3fc21c4fe22cb7feb702d
Gabriel Levy <gabelevy@gmail.com>
2026-02-23 22:50:46 -0800

    memory tuning

--- memory.cpp
+++ memory.cpp
@@ -2,5 +2,5 @@
 
 // Allocations
 Arena geometry_arena(GEOMETRY_ARENA_SIZE);
-Arena scratch_arena_a(SCRATCH_ARENA_SIZE);
-Arena scratch_arena_b(SCRATCH_ARENA_SIZE);
+Arena scratch_arena_a(SCRATCH_ARENA_A_SIZE);
+Arena scratch_arena_b(SCRATCH_ARENA_B_SIZE);
