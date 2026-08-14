12270b8a0a0914a5d084411ca6b5161919c653fb
Gabriel Levy <gabelevy@gmail.com>
2026-08-14 11:24:31 -0700

    tests: account for ShaderBall warp resource guards

--- tests/test_death.h
+++ tests/test_death.h
@@ -3390,7 +3390,7 @@ inline constexpr GuardGapAllowance GUARD_GAP_ALLOW[] = {
     {"MobiusGrid.h", 1},
     {"ReactionDiffusionBase.h", 2},
     {"RingShower.h", 1},
-    {"ShaderBall.h", 14},
+    {"ShaderBall.h", 16},
     {"ShapeShifter.h", 3},
     {"dma_led.h", 4},
     {"pov_segmented.h", 9},
