0592cadbe49c1ff2e193ad07be33f34a18625c47
Gabriel Levy <gabelevy@gmail.com>
2026-08-24 12:47:19 -0700

    HyperLattice: inline periodic lattice folding

--- effects/HyperLattice.h
+++ effects/HyperLattice.h
@@ -78,7 +78,7 @@ inline void rotate_plane(Mat4 &matrix, int a, int b, float angle) {
   }
 }
 
-inline float periodic_distance(float value) {
+__attribute__((always_inline)) inline float periodic_distance(float value) {
   return fabsf(value - floorf(value + 0.5f));
 }
 
