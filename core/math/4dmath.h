a07620d826a4f34524c7826d1b6dfd8b843a6df7
Gabriel Levy <gabelevy@gmail.com>
2026-09-24 00:55:49 -0700

    Place shared rotation in a COMDAT-safe cold section (finding 114)

--- core/math/4dmath.h
+++ core/math/4dmath.h
@@ -64,7 +64,8 @@ struct Mat4 {
  * @param b Second plane axis, in [0, VEC4_DIMENSIONS), distinct from `a`.
  * @param angle Rotation angle in radians.
  */
-HS_COLD inline void rotate_plane(Mat4 &matrix, int a, int b, float angle) {
+HS_COLD_MEMBER inline void rotate_plane(Mat4 &matrix, int a, int b,
+                                        float angle) {
   HS_CHECK(a >= 0 && a < VEC4_DIMENSIONS && b >= 0 && b < VEC4_DIMENSIONS &&
                a != b,
            "rotate_plane: (%d, %d) is not a coordinate plane", a, b);
